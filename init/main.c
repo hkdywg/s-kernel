36c9b2ca10aefce217cd48a85590923dd155dd5e
hkdywg <hkdywg@163.com>
2024-12-27 17:41:50 +0800

    Add: kernel framework file, but no source code
    Signed-off-by: hkdywg <hkdywg@163.com>

---
+++ init/main.c
@@ -0,0 +1,18 @@
+/*
+ *  main.c
+ *  brief
+ *  	kernel entrance
+ *  
+ *  (C) 2024.12.25 <hkdywg@163.com>
+ *
+ *  This program is free software; you can redistribute it and/r modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ * */
+#include <stdio.h>
+
+int main()
+{
+	printf("run kernel os\n");
+	return 0;
+}
