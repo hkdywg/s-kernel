5465ff8cee721c7cd987aae7e585e08e491f6662
hkdywg <hkdywg@163.com>
2025-01-16 19:34:02 +0800

    Update: support memory management init, and add test case
    Signed-off-by: hkdywg <hkdywg@163.com>

--- init/main.c
+++ init/main.c
@@ -9,29 +9,44 @@
  *  it under the terms of the GNU General Public License version 2 as
  *  published by the Free Software Foundation.
  * */
-#include <type_def.h>
+#include <interrupt.h>
+#include <skernel.h>
+#include <hw.h>
+#include <skernel.h>
 
-int func_1() 
+extern unsigned char __bss_start;
+extern unsigned char __bss_end;
+
+#define SK_HEAP_BEGIN    (void*)&__bss_end
+#define SK_HEAP_END      (void*)(SK_HEAP_BEGIN + 1 * 1024 * 1024)
+
+/*
+ * Init the hardware related 
+ *
+ * @param: none
+ */
+void sk_hw_board_init(void)
 {
-	sk_uint32_t cnt = SK_UINT32_MAX;
-	while(cnt--);
+	/* Initialize hardware interrupt */
+	sk_hw_interrupt_init();
 
-	return 0;
-}
+	/* kernel tick init */
+	sk_hw_timer_init();
 
-int func_2() 
-{
-	sk_uint32_t cnt = SK_UINT32_MAX;
-	while(cnt--);
-
-	return 0;
+	/* memory management init */
+	sk_system_mem_init(SK_HEAP_BEGIN, SK_HEAP_END);
 }
 
 int skernel_startup(void)
 {
-	while(1) {
-		func_1();
-		func_2();
-	}
+	/* hardware related init, must be first called in skernel_startup*/
+	sk_hw_board_init();
+
+	sk_uint8_t *ptr_1 = SK_NULL;
+	ptr_1 = (sk_uint8_t *)sk_malloc(15);
+	*ptr_1 = 12;
+	sk_uint8_t test_var = 0xa5;
+	while(1);
+
 	return 0;
 }
