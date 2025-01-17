c1f38f1f289f117c5e85832fc6ccbc84594b28e2
hkdywg <hkdywg@163.com>
2025-01-17 16:09:11 +0800

    Add: kernel object management function
    Signed-off-by: hkdywg <hkdywg@163.com>

---
+++ include/kobj.h
@@ -0,0 +1,56 @@
+/*
+ *  kobj.h
+ *  brief
+ *  	kernel object definitions of s-kernel 
+ *  
+ *  (C) 2025.01.17 <hkdywg@163.com>
+ *
+ *  This program is free software; you can redistribute it and/r modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ */
+#ifndef __KOBJ_H_
+#define __KOBJ_H_
+
+#include <base_def.h>
+#include <klist.h>
+
+/* 
+ * basic structure of kernel object 
+ */
+struct sk_object
+{
+	char 		name[SK_NAME_MAX];			/* name of kernel object */
+	sk_uint8_t  type;						/* type of kernel object */
+	sk_uint8_t  flag;						/* flag of kernel object */
+
+	sk_list_t  	list;						/* list node of kernel object */ 				
+};
+
+
+/*
+ * define object_type 
+ */
+enum sk_object_type {
+	SK_OBJECT_THREAD = 0,					/* thread object */
+	SK_OBJECT_SEMAPHORE, 					/* semaphore object */
+	SK_OBJECT_MUTEX,	 					/* mutex object */
+	SK_OBJECT_EVENT,	 					/* event object */
+	SK_OBJECT_MAILBOX,	 					/* mailbox object */
+	SK_OBJECT_MSQUE,	 					/* message queue object */
+	SK_OBJECT_DEVICE,	 					/* device object */
+	SK_OBJECT_TIMER,	 					/* tick object */
+	SK_OBJECT_UNKNOWN,
+};
+
+/*
+ * the information of the kernel object
+ */
+struct sk_object_info
+{
+	enum sk_object_type type;				/* object class type */
+	sk_list_t 			obj_list; 			/* object list */
+	sk_size_t 			obj_size;			/* object size */
+};
+
+#endif
---
+++ init/kobj.c
@@ -0,0 +1,111 @@
+/*
+ *  kobj.c
+ *  brief
+ *  	object definitions, used by ipc and thread and device
+ *  
+ *  (C) 2025.01.17 <hkdywg@163.com>
+ *
+ *  This program is free software; you can redistribute it and/r modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ * */
+#include <skernel.h>
+#include <kobj.h>
+#include <timer.h>
+#include <interrupt.h>
+
+/* init the sk_object double list */
+#define _OBJ_CONTAINER_LIST_INIT(c)	\
+	{&(_object_container[c].obj_list), &(_object_container[c].obj_list)}
+
+static struct sk_object_info _object_container[] = {
+	{SK_OBJECT_TIMER, _OBJ_CONTAINER_LIST_INIT(SK_OBJECT_TIMER), sizeof(struct sk_sys_timer)},
+};
+
+
+/*
+ * sk_object_get_info
+ * brief
+ * 		this function will return the specified type  of object information
+ * param
+ * 		type: the object type
+ */
+struct sk_object_info *sk_object_get_info(enum sk_object_type type)
+{
+	int index;
+	for(index = 0; index < SK_OBJECT_UNKNOWN; index++) {
+		if(_object_container[index].type == type)
+			return &_object_container[index];
+	}
+
+	return SK_NULL;
+}
+
+/*
+ *	sk_object_alloc
+ *	brief
+ *		allocate an object from object system
+ *	param
+ *		type: the type of object
+ *		name: the name of object name
+ */
+struct sk_object *sk_object_alloc(enum sk_object_type type, const char *name) 
+{
+	struct sk_object *obj;
+	struct sk_object_info  *info;
+	sk_base_t level;
+
+	/* get object information */
+	info = sk_object_get_info(type);
+
+	obj = (struct sk_object *)sk_malloc(info->obj_size);
+	if(obj == SK_NULL)
+		return SK_NULL;
+
+	/* clean memory data of object */
+	sk_memset(obj, 0, info->obj_size);
+
+	/* initialize object's paramters */
+	obj->type = type;
+	obj->flag = 0;
+
+	/* copy name */
+	sk_memcpy(obj->name, name, SK_NAME_MAX);
+
+	/* disable interrupt */
+	level = hw_interrupt_disable();
+
+	/* insert object into information object list */
+	sk_list_add(&(info->obj_list), &(obj->list));
+
+	/* enable interrupt */
+	hw_interrupt_enable(level);
+
+	return obj;
+}
+
+/*
+ * sk_object_detach
+ * brief
+ * 		detach a static object from object system
+ * param
+ * 		obj: the object of need to be detached
+ */
+void sk_object_detach(struct sk_object *obj)
+{
+	sk_base_t level;
+
+	/* reset object type */
+	obj->type = -1;
+
+	/* disable interrupt */
+	level = hw_interrupt_disable();
+
+	/* insert object into information object list */
+	sk_list_del(&(obj->list));
+
+	/* enable interrupt */
+	hw_interrupt_enable(level);
+
+}
+
