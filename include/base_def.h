371b85f730175c16905d365026882e99dcddeec1
hkdywg <hkdywg@163.com>
2025-01-22 14:15:58 +0800

    Update: basic function definition
    Signed-off-by: hkdywg <hkdywg@163.com>

--- include/base_def.h
+++ include/base_def.h
@@ -88,6 +88,7 @@ typedef sk_base_t 					sk_off_t;		/* type for offset */
  */
 #define SK_ALIGN_DOWN(size, align)	((size) & ~((align) -1 ))
 
+
 /*
  * sk_memset
  * brief
@@ -131,6 +132,53 @@ static inline void *sk_memcpy(void *dst, const void *src, sk_ubase_t count)
 	}
 	return dst;
 }
+
+                                                                                                                                                                      
+/**                                                                                                                                                                   
+ * This function finds the first bit set (beginning with the least significant bit)                                                                                   
+ * in value and return the index of that bit.                                                                                                                         
+ *                                                                                                                                                                    
+ * Bits are numbered starting at 1 (the least significant bit).  A return value of                                                                                    
+ * zero from any of these functions means that the argument was zero.                                                                                                 
+ *                                                                                                                                                                    
+ * @return Return the index of the first bit set. If value is 0, then this function                                                                                   
+ *         shall return 0.                                                                                                                                            
+ */                                                                                                                                                                   
+static inline int __sk_ffs(int value)                                                                                                                                               
+{                                                                                                                                                                     
+	static const sk_uint8_t __lowest_bit_bitmap[] =                                                                                                                              
+	{                                                                                                                                                                     
+		/* 00 */ 0, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* 10 */ 4, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* 20 */ 5, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* 30 */ 4, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* 40 */ 6, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* 50 */ 4, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* 60 */ 5, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* 70 */ 4, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* 80 */ 7, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* 90 */ 4, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* A0 */ 5, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* B0 */ 4, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* C0 */ 6, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* D0 */ 4, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* E0 */ 5, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,                                                                                                          
+		/* F0 */ 4, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0                                                                                                           
+	};                                                                                                                                                                    
+
+    if (value == 0) return 0;                                                                                                                                         
+                                                                                                                                                                      
+    if (value & 0xff)                                                                                                                                                 
+        return __lowest_bit_bitmap[value & 0xff] + 1;                                                                                                                 
+                                                                                                                                                                      
+    if (value & 0xff00)                                                                                                                                               
+        return __lowest_bit_bitmap[(value & 0xff00) >> 8] + 9;                                                                                                        
+                                                                                                                                                                      
+    if (value & 0xff0000)                                                                                                                                             
+        return __lowest_bit_bitmap[(value & 0xff0000) >> 16] + 17;                                                                                                    
+                                                                                                                                                                      
+    return __lowest_bit_bitmap[(value & 0xff000000) >> 24] + 25;                                                                                                      
+} 
 
 #endif
 
