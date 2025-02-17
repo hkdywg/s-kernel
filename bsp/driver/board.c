3b77dd414ecc89ee99c99ce3a1bf1e09e306c584
hkdywg <hkdywg@163.com>
2025-02-17 17:36:22 +0800

    Update: support formatted string output
    Signed-off-by: hkdywg <hkdywg@163.com>

--- bsp/driver/board.c
+++ bsp/driver/board.c
@@ -14,6 +14,19 @@
 #include <config.h>
 #include <board.h>
 
+void sk_show_version(void) {
+    sk_kprintf("      .-'`'-.        \n");
+    sk_kprintf("    /        \\      \n");
+    sk_kprintf("   | s-kernel |     \n");
+    sk_kprintf("   | Operation|     \n");
+    sk_kprintf("   | System   |     \n");
+    sk_kprintf("    \\________/      \n");
+    sk_kprintf("      V %d.%d.%d      \n", SK_VERSION, SK_SUBVERSION, SK_REVISION);
+    sk_kprintf("   Build: %s %s\n", __DATE__, __TIME__);
+    sk_kprintf("   Copyright 2024-2025\n");
+    sk_kprintf("   Powered by yinwg\n");
+}
+
 /*
  * Init the hardware related 
  *
@@ -28,6 +41,7 @@ void sk_hw_board_drv_init(void)
 	sk_console_set_device(SK_CONSOLE_DEVICE_NAME);
 
 	/**/
-	sk_kprintf("------skernel--------\n");
+	//sk_kprintf("------skernel-------- %d\n", 123);
+	sk_show_version();
 }
 
--- driver/serial/console.c
+++ driver/serial/console.c
@@ -61,12 +61,128 @@ struct sk_device *sk_console_get_device(void)
 	return  _console_device;
 }
 
+sk_size_t skip_atoi(const char **s)
+{
+	sk_size_t i = 0;
+	while(SK_ISDIGIT(**s))
+		i = i * 10 + *((*s)++) - '0';
+
+	return i;
+}
+
+static void sk_itoa(int num, char *str, int base)
+{
+	sk_uint32_t i = 0;
+	sk_uint32_t rem;
+	sk_bool_t is_negative = SK_FALSE;
+
+	/* handle 0 explicityly */
+	if(num == 0) {
+		str[i++] = '0';
+		str[i] = '\0';
+		return;
+	}
+	/* handle negative numbers only if base is 10 */
+	if(num < 0 && base == 10) {
+		is_negative = SK_TRUE;
+		num = -num;
+	}
+	/* process individual digits */
+	while(num != 0) {
+		rem = num % base;
+		str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+		num = num / base;
+	}
+	/* append negative sign for negative numbers */
+	if(is_negative)
+		str[i++] = '-';
+	str[i] = '\0';
+
+	/* reverse the string */
+	sk_uint32_t start = 0;
+	sk_uint32_t end = i - 1;
+	while(start < end) {
+		char temp = str[start];
+		str[start] = str[end];
+		str[end] = temp;
+		start++;
+		end--;
+	}
+}
+
+/*
+ * sk_vsprintf
+ * brief
+ * 		this function will fill a formatted string to buffer
+ * param
+ * 		buf: the buffer to save formatted string
+ * 		fmt: the format parameters
+ * 		args: a list of variable parameters
+ */
+static sk_size_t sk_vsprintf(char *buf,
+							 const char *fmt, va_list args)
+{
+	char *str = buf;
+	const char *p = fmt;
+	char temp[32];		/* temp buffer */
+
+	while(*p) {
+		if(*p == '%' && *(p + 1)) {
+			p++;	/* skip '%' */
+			switch(*p) {
+			case 'd':
+			{
+				int val = va_arg(args, int);
+				sk_itoa(val, temp, 10);
+				sk_strcpy(str, temp);
+				str += sk_strlen(temp);
+			}
+			break;
+			case 'x':
+			{
+				int val = va_arg(args, int);
+				sk_itoa(val, temp, 16);
+				sk_strcpy(str, temp);
+				str += sk_strlen(temp);
+			}
+			break;
+			case 's':
+			{
+				char *val = va_arg(args, char*);
+				sk_strcpy(str, val);
+				str += sk_strlen(val);
+			}
+			break;
+			case 'c':
+			{
+				char val = (char)va_arg(args, int);
+				*str = val;
+				str++;
+			}
+			break;
+			default:
+				*str = '%';
+				str++;
+				*str = *p;
+				str++;
+				break;
+			}
+		} else {
+			*str = *p;
+			str++;
+		}
+		p++;
+	}
+	*str = '\0';
+	return str - buf;
+}
+
 /*
  * sk_kprintf
  * brief
  * 		this function will print a formatted string on system console.
  */
-#if 0
+#if 1
 void sk_kprintf(const char *fmt, ...)
 {
 	va_list args;
@@ -74,10 +190,10 @@ void sk_kprintf(const char *fmt, ...)
 	static char sk_log_buf[SK_CONSOLE_BUF_SIZE];
 
 	va_start(args, fmt);
-//	length = vsprintf(sk_log_buf, fmt, args);
+	length = sk_vsprintf(sk_log_buf, fmt, args);
 	if(length > SK_CONSOLE_BUF_SIZE)
 		length = SK_CONSOLE_BUF_SIZE;
-	if(!_console_device)
+	if(_console_device)
 		sk_device_write(_console_device, 0, sk_log_buf, length);
 
 	va_end(args);
