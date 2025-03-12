4b260987154418b7cae831834d173c1c3d29920b
hkdywg <hkdywg@163.com>
2025-03-12 14:58:53 +0800

    Update: test case
    Signed-off-by: hkdywg <hkdywg@163.com>

--- user/main.c
+++ user/main.c
@@ -13,107 +13,23 @@
 #include <timer.h>
 #include <ipc.h>
 #include <shell.h>
-
-//#define TEST_TIMER
-//#define TEST_MUTEX
-//#define TEST_SEMAPHORE
-//#define TEST_EVENT
-
-struct sk_mutex g_mutex;
-struct sk_sem 	g_sem;
-struct sk_event g_event;
-
-sk_uint8_t timer1_cnt = 0; 
-sk_uint8_t timer2_cnt = 0; 
-
-#define EVENT_FLAG0 (1 << 0)
-#define EVENT_FLAG1 (1 << 1)
-
-void user_app_1(void *arg)
-{
-	while(1) {
-#ifdef TEST_SEMAPHORE
-		sk_sem_wait(&g_sem, 10000);
-		sk_kprintf("%s take sem\n", sk_current_thread()->name);	
-		sk_thread_delay(100);
-		sk_kprintf("%s release sem\n", sk_current_thread()->name);	
-		sk_sem_post(&g_sem);
-		sk_thread_delay(1000);
-#endif
-#ifdef TEST_EVENT
-		sk_thread_delay(10);
-		sk_event_send(&g_event, EVENT_FLAG0);
-		sk_thread_delay(10);
-		sk_event_send(&g_event, EVENT_FLAG1);
-		sk_thread_delay(1000);
-		sk_event_send(&g_event, EVENT_FLAG1);
-		sk_thread_delay(10000);
-#endif
-	}
-}
-
-void user_app_2(void *arg)
-{
-	while(1) {
-#ifdef TEST_SEMAPHORE
-		sk_sem_wait(&g_sem, 10000);
-		sk_kprintf("%s take sem\n", sk_current_thread()->name);	
-		sk_thread_delay(100);
-		sk_kprintf("%s release sem\n", sk_current_thread()->name);	
-		sk_sem_post(&g_sem);
-		sk_thread_delay(1000);
-#endif
-#ifdef TEST_EVENT
-	sk_uint32_t e;
-	if(sk_event_recv(&g_event, (EVENT_FLAG0 | EVENT_FLAG1),
-					 SK_EVENT_FLAG_OR | SK_EVENT_FLAG_CLEAR,
-					 -1, &e) == SK_EOK) {
-		sk_kprintf("%s recv event: %d\n", sk_current_thread()->name, e);
-	}
-	sk_thread_delay(100);
-	if(sk_event_recv(&g_event, (EVENT_FLAG0 | EVENT_FLAG1),
-					 SK_EVENT_FLAG_AND | SK_EVENT_FLAG_CLEAR,
-					 -1, &e) == SK_EOK) {
-		sk_kprintf("%s recv event: %d\n", sk_current_thread()->name, e);
-	}
-#endif
-	}
-}
-
-void user_app_init(void)
-{
-	sk_sem_init(&g_sem, "test_sem", 2, SK_IPC_FLAG_PRIO);
-	sk_event_init(&g_event, "test_event", SK_IPC_FLAG_PRIO);
-
-	char user_thread_name_1[SK_NAME_MAX] = "user_app_1";
-	struct sk_thread *thread_1 = sk_thread_create(user_thread_name_1,
-												user_app_1,
-												SK_NULL,
-												2048,
-												20,
-												20);
-	sk_thread_startup(thread_1);
-	char user_thread_name_2[SK_NAME_MAX] = "user_app_2";
-	struct sk_thread *thread_2 = sk_thread_create(user_thread_name_2,
-												user_app_2,
-												SK_NULL,
-												2048,
-												20,
-												20);
-	sk_thread_startup(thread_2);
-}
+	
+static float cpu_usage = 0.0;
 
 void main(void *arg)
 {
 	sk_ubase_t cnt = 0;
-	float cpu_usage = 0.0;
-
-//	user_app_init();
 
 	while(1) {
 		cnt++;
-		cpu_usage = sk_idle_tick_get()/sk_tick_get();
+		cpu_usage = (1.0 - (float)sk_idle_tick_get()/(float)sk_tick_get());
 		sk_thread_delay(1000);
-//    	sk_kprintf("%s thread runing ...\n", sk_current_thread()->name);
 	}
 }
+
+void show_cpu_usage()
+{
+	sk_kprintf("current cpu usage is %d\%\n", (sk_uint8_t)(cpu_usage * 100));
+}
+
+SHELL_CMD_EXPORT(show_cpu_usage, test case of ipc semaphore);
--- user/test_case.c
+++ user/test_case.c
@@ -14,6 +14,8 @@
 #include <ipc.h>
 #include <shell.h>
 	
+#define EVENT_FLAG0 (1 << 0)
+#define EVENT_FLAG1 (1 << 1)
 
 void test_timer_func(void *param)
 {
@@ -43,8 +45,8 @@ SHELL_CMD_EXPORT(test_sys_timer, test case of system timer);
 void mutex_thread_1(void *param)
 {
 	struct sk_mutex *mutex = (struct sk_mutex *)param;
-	sk_mutex_lock(mutex, 10000);
 	sk_kprintf("%s take mutex\n", sk_current_thread()->name);	
+	sk_mutex_lock(mutex, 10000);
 	sk_thread_delay(2000);
 	sk_mutex_unlock(mutex);
 	sk_kprintf("%s release mutex\n", sk_current_thread()->name);	
@@ -53,8 +55,8 @@ void mutex_thread_1(void *param)
 void mutex_thread_2(void *param)
 {
 	struct sk_mutex *mutex = (struct sk_mutex *)param;
-	sk_mutex_lock(mutex, -1);
 	sk_kprintf("%s take mutex\n", sk_current_thread()->name);	
+	sk_mutex_lock(mutex, -1);
 	sk_thread_delay(100);
 	sk_mutex_unlock(mutex);
 	sk_kprintf("%s release mutex\n", sk_current_thread()->name);	
@@ -85,3 +87,99 @@ void test_mutex(void)
 
 SHELL_CMD_EXPORT(test_mutex, test case of ipc mutex);
 
+void sem_thread_1(void *param)
+{
+	struct sk_sem *sem = (struct sk_sem *)param;
+	sk_kprintf("%s wait semaphore\n", sk_current_thread()->name);	
+	sk_sem_wait(sem, 10000);
+	sk_thread_delay(2000);
+	sk_sem_post(sem);
+	sk_kprintf("%s post semaphore \n", sk_current_thread()->name);	
+}
+
+void sem_thread_2(void *param)
+{
+	struct sk_sem *sem = (struct sk_sem *)param;
+	sk_sem_wait(sem, -1);
+	sk_kprintf("%s get semaphore\n", sk_current_thread()->name);	
+	sk_thread_delay(100);
+	sk_sem_post(sem);
+	sk_kprintf("%s post semaphore \n", sk_current_thread()->name);	
+}
+
+void test_sem(void)
+{
+	static struct sk_sem sem;
+	sk_sem_init(&sem, "test_sem", 2, SK_IPC_FLAG_PRIO);
+
+	char thread_name_1[SK_NAME_MAX] = "sem_thread_1";
+	struct sk_thread *thread_1 = sk_thread_create(thread_name_1,
+												sem_thread_1,
+												&sem,
+												2048,
+												20,
+												20);
+	sk_thread_startup(thread_1);
+	char thread_name_2[SK_NAME_MAX] = "mutex_thread_2";
+	struct sk_thread *thread_2 = sk_thread_create(thread_name_2,
+												sem_thread_2,
+												&sem,
+												2048,
+												20,
+												20);
+	sk_thread_startup(thread_2);
+}
+
+SHELL_CMD_EXPORT(test_sem, test case of ipc semaphore);
+
+
+void event_thread_1(void *param)
+{
+	struct sk_event *event = (struct sk_event *)param;
+	sk_thread_delay(100);
+	sk_event_send(event, EVENT_FLAG1);
+	sk_thread_delay(100);
+	sk_event_send(event, EVENT_FLAG0);
+}
+
+void event_thread_2(void *param)
+{
+	struct sk_event *event = (struct sk_event *)param;
+	sk_uint32_t e;
+
+	if(sk_event_recv(event, (EVENT_FLAG0 | EVENT_FLAG1),
+					 SK_EVENT_FLAG_OR,
+					 -1, &e) == SK_EOK) {
+		sk_kprintf("%s recv event: %d\n", sk_current_thread()->name, e);
+	}
+	if(sk_event_recv(event, (EVENT_FLAG0 | EVENT_FLAG1),
+					 SK_EVENT_FLAG_AND | SK_EVENT_FLAG_CLEAR,
+					 -1, &e) == SK_EOK) {
+		sk_kprintf("%s recv event: %d\n", sk_current_thread()->name, e);
+	}
+}
+
+void test_event(void)
+{
+	static struct sk_event event; 
+	sk_event_init(&event, "test_event", SK_IPC_FLAG_PRIO);
+
+	char thread_name_1[SK_NAME_MAX] = "sem_thread_1";
+	struct sk_thread *thread_1 = sk_thread_create(thread_name_1,
+												event_thread_1,
+												&event,
+												2048,
+												20,
+												20);
+	sk_thread_startup(thread_1);
+	char thread_name_2[SK_NAME_MAX] = "mutex_thread_2";
+	struct sk_thread *thread_2 = sk_thread_create(thread_name_2,
+												event_thread_2,
+												&event,
+												2048,
+												20,
+												20);
+	sk_thread_startup(thread_2);
+}
+
+SHELL_CMD_EXPORT(test_event, test case of ipc semaphore);
